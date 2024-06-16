eeef6760fc26c1b4585503cdfb3d9e80ccdc2366
JecianeSilva <jeciane_amorim@hotmail.com>
2024-06-16 17:06:37 -0300

    Feat: adjustment database ans class Employe

--- src/main/java/br/com/project/api/model/Employee.java
+++ src/main/java/br/com/project/api/model/Employee.java
@@ -3,12 +3,23 @@ package br.com.project.api.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "Employee")
 public class Employee extends Person {
   private BigDecimal salary;
   private String role;
+
+  public Employee() {
+  }
+
+  public Employee(String name, LocalDate dateBirth, BigDecimal salary, String role) {
+    setName(name);
+    setDateBirth(dateBirth);
+    this.salary = salary;
+    this.role = role;
+  }
 
   // Getters e Setters
   public BigDecimal getSalary() {
